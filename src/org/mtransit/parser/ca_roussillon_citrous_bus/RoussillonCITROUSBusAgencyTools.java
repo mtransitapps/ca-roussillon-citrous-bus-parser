0f8dae85d72e3096a05f34d65789965140fbc840
Mathieu Méa <mathieu.mea@gmail.com>
2015-03-16 14:25:00 -0400

    Minor

--- src/org/mtransit/parser/ca_roussillon_citrous_bus/RoussillonCITROUSBusAgencyTools.java
+++ src/org/mtransit/parser/ca_roussillon_citrous_bus/RoussillonCITROUSBusAgencyTools.java
@@ -100,7 +100,7 @@ public class RoussillonCITROUSBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public void setTripHeadsign(MRoute route, MTrip mTrip, GTrip gTrip) {
 		String stationName = cleanTripHeadsign(gTrip.trip_headsign);
-		int directionId = Integer.valueOf(gTrip.direction_id);
+		int directionId = gTrip.direction_id;
 		if (mTrip.getRouteId() == 33l) {
 			if (directionId == 0) {
 				stationName = "Stationnement Georges-Gagné";
