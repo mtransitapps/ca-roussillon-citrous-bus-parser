eaf50fafec5935a3c2b418a737f6069b99501f09
Mathieu Méa <mathieu.mea@gmail.com>
2016-03-06 09:06:04 -0500

    Improve trip head signs.

--- src/org/mtransit/parser/ca_roussillon_citrous_bus/RoussillonCITROUSBusAgencyTools.java
+++ src/org/mtransit/parser/ca_roussillon_citrous_bus/RoussillonCITROUSBusAgencyTools.java
@@ -1,9 +1,17 @@
 package org.mtransit.parser.ca_roussillon_citrous_bus;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
 import java.util.regex.Pattern;
 
+import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.Pair;
+import org.mtransit.parser.SplitUtils;
+import org.mtransit.parser.SplitUtils.RouteTripSpec;
 import org.mtransit.parser.Utils;
 import org.mtransit.parser.gtfs.data.GCalendar;
 import org.mtransit.parser.gtfs.data.GCalendarDate;
@@ -11,10 +19,11 @@ import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GSpec;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
+import org.mtransit.parser.gtfs.data.GTripStop;
 import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRoute;
-import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.mt.data.MTrip;
+import org.mtransit.parser.mt.data.MTripStop;
 
 // http://www.amt.qc.ca/developers/
 // http://www.amt.qc.ca/xdata/citrous/google_transit.zip
@@ -104,28 +113,83 @@ public class RoussillonCITROUSBusAgencyTools extends DefaultAgencyTools {
 		return AGENCY_COLOR;
 	}
 
-	private static final String LONGUEUIL_DELSON = "Longueuil-Delson";
-	private static final String DELSON_LONGUEUIL_DELSON = "Delson-Longueuil-Delson";
-	private static final String GARE_STE_CATHERINE = "Gare Ste-Catherine";
-	private static final String STATIONNEMENT_GEORGES_GAGNÉ = "Stationnement Georges-Gagné";
+	private static final String AM = "AM";
+	private static final String PM = "PM";
+	private static final String DELSON = "Delson";
+
+	private static HashMap<Long, RouteTripSpec> ALL_ROUTE_TRIPS2;
+	static {
+		HashMap<Long, RouteTripSpec> map2 = new HashMap<Long, RouteTripSpec>();
+		ALL_ROUTE_TRIPS2 = map2;
+	}
+
+	@Override
+	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
+		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
+			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+		}
+		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+	}
+
+	@Override
+	public ArrayList<MTrip> splitTrip(MRoute mRoute, GTrip gTrip, GSpec gtfs) {
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return ALL_ROUTE_TRIPS2.get(mRoute.getId()).getAllTrips();
+		}
+		return super.splitTrip(mRoute, gTrip, gtfs);
+	}
+
+	@Override
+	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()));
+		}
+		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
+	}
 
 	@Override
 	public void setTripHeadsign(MRoute mRoute, MTrip mTrip, GTrip gTrip, GSpec gtfs) {
-		String stationName = cleanTripHeadsign(gTrip.getTripHeadsign());
-		if (mTrip.getRouteId() == 33l) {
+		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
+			return; // split
+		}
+		if (gTrip.getTripHeadsign().toUpperCase(Locale.ENGLISH).endsWith(AM)) {
+			mTrip.setHeadsignString(AM, gTrip.getDirectionId());
+			return;
+		} else if (gTrip.getTripHeadsign().toUpperCase(Locale.ENGLISH).endsWith(PM)) {
+			mTrip.setHeadsignString(PM, gTrip.getDirectionId());
+			return;
+		}
+		if (mTrip.getRouteId() == 210l) {
 			if (gTrip.getDirectionId() == 0) {
-				stationName = STATIONNEMENT_GEORGES_GAGNÉ;
-			} else {
-				stationName = GARE_STE_CATHERINE;
-			}
-		} else if (mTrip.getRouteId() == 210l) {
-			if (gTrip.getDirectionId() == 0) {
-				stationName = DELSON_LONGUEUIL_DELSON;
-			} else {
-				stationName = LONGUEUIL_DELSON;
+				mTrip.setHeadsignString(AM, gTrip.getDirectionId());
+				return;
+			} else if (gTrip.getDirectionId() == 1) {
+				mTrip.setHeadsignString(PM, gTrip.getDirectionId());
+				return;
 			}
 		}
-		mTrip.setHeadsignString(stationName, gTrip.getDirectionId());
+		mTrip.setHeadsignString(cleanTripHeadsign(gTrip.getTripHeadsign()), gTrip.getDirectionId());
+	}
+
+	@Override
+	public boolean mergeHeadsign(MTrip mTrip, MTrip mTripToMerge) {
+		if (mTrip.getRouteId() == 33l) {
+			if (mTrip.getHeadsignId() == 0) {
+				mTrip.setHeadsignString(PM, mTrip.getHeadsignId());
+				return true;
+			} else if (mTrip.getHeadsignId() == 1) {
+				mTrip.setHeadsignString(AM, mTrip.getHeadsignId());
+				return true;
+			}
+		} else if (mTrip.getRouteId() == 200l) {
+			if (mTrip.getHeadsignId() == 1) {
+				mTrip.setHeadsignString(DELSON, mTrip.getHeadsignId());
+				return true;
+			}
+		}
+		System.out.printf("\nUnexpected trips to merge %s & %s!\n", mTrip, mTripToMerge);
+		System.exit(-1);
+		return false;
 	}
 
 	private static final Pattern DIRECTION = Pattern.compile("(direction )", Pattern.CASE_INSENSITIVE);
